7ae1f05c05513a1b575f7a26a46601c0f6877805
mthu06103-spec <mthu06103@gmail.com>
2026-08-19 09:57:37 +0700

    Create run_telemetry.py for telemetry analysis
    Add a script to run telemetry analysis for the Data Science demo.

---
+++ run_telemetry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Script chạy Phân tích Telemetry Thực tế
+Cảng Container Cát Lái - Data Science Demo
+"""
+
+if __name__ == "__main__":
+    # Import module
+    from data_science.telemetry_analyzer import run_telemetry_analysis
+    
+    # Chạy phân tích
+    run_telemetry_analysis()
